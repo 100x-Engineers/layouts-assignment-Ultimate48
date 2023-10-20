a9133791e4a01a2bde3fc7b757684fc2235be2d8
Adeesh Garg <adeesh.g47@gmail.com>
2023-10-20 13:27:40 +0530

    login index page complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      'Inter':['Inter','sans-serif'],
+      'Chirp':['Chirp','sans-serif'],
+    },
+    extend: {
+      boxShadow:{
+        'standard':'0px 8px 16px 0px rgba(0, 0, 0, 0.25)',
+      },
+      colors:{
+        'neutral-1000':"#000",
+          'neutral-50':'#F9F9F9',
+          "buttonstroke":"#546a7a",
+          "twitterblue-default":"#1d9bf0",
+        
+      }
+    },
   },
   plugins: [],
 };
